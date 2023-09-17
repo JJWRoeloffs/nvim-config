b4908881d224b69b2d09733b1dfc25a78a8ce511
JJWRoeloffs <jelleroeloffs@gmail.com>
2023-09-17 21:33:33 +0200

    clean up LSP-zero configuration

--- lua/plugins/lsp-zero.lua
+++ lua/plugins/lsp-zero.lua
@@ -43,24 +43,20 @@ local function setup()
         "jsonls",
     })
 
-    -- Fix Undefined global 'vim'
-    lsp.configure("lua-language-server", {
-        settings = {
-            Lua = {
-                diagnostics = {
-                    globals = { "vim" },
-                },
-            },
+    require("neodev").setup({
+        library = {
+            plugins = vim.tbl_keys(require("lazy.core.config").plugins),
+            type = true,
         },
     })
 
     local cmp = require("cmp")
-    local cmp_mappings = lsp.defaults.cmp_mappings({
-        ["<C-Space>"] = cmp.mapping.complete(),
+    local cmp_mappings = cmp.mapping.preset.insert({
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
     })
-
-    cmp_mappings["<Tab>"] = nil
-    cmp_mappings["<S-Tab>"] = nil
 
     lsp.setup_nvim_cmp({
         mapping = cmp_mappings,
@@ -99,10 +95,34 @@ return {
         { "hrsh7th/cmp-path" },
         { "saadparwaiz1/cmp_luasnip" },
         { "hrsh7th/cmp-nvim-lua" },
+        { "folke/lsp-colors.nvim" },
 
         -- Snippets
         { "L3MON4D3/LuaSnip" },
         { "rafamadriz/friendly-snippets" },
+
+        -- nvim configuring
+        { "folke/neodev.nvim", opts = {} },
+        { "folke/trouble.nvim", config = require("plugins.trouble").setup },
+    },
+    ft = {
+        "lua",
+        "Javascript",
+        "typescript",
+        "html",
+        "css",
+        "python",
+        "rust",
+        "cpp",
+        "latex",
+        "docker",
+        "sql",
+        "powershell",
+        "sh",
+        "yaml",
+        "toml",
+        "json",
+        "xml",
     },
     config = setup,
 }
