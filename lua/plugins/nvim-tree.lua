35b72126571d178d998f6f8c1d621d08d7929503
JJWRoeloffs <jelleroeloffs@gmail.com>
2023-09-17 21:32:32 +0200

    Add new nvim-tree config

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,11 +1,49 @@
 local function setup()
     require("nvim-tree").setup({
-        sort_by = "case_sensitive",
+        sync_root_with_cwd = true,
         renderer = {
+            full_name = true,
             group_empty = true,
+            special_files = {},
+            symlink_destination = false,
+            indent_markers = {
+                enable = true,
+            },
+            icons = {
+                git_placement = "signcolumn",
+                show = {
+                    file = true,
+                    folder = false,
+                    folder_arrow = true,
+                    git = true,
+                },
+            },
+        },
+        update_focused_file = {
+            enable = true,
+            update_root = true,
+            ignore_list = { "help" },
+        },
+        diagnostics = {
+            enable = true,
+            show_on_dirs = true,
         },
         filters = {
-            dotfiles = true,
+            custom = {
+                "^.git$",
+            },
+        },
+        actions = {
+            change_dir = {
+                enable = false,
+                restrict_above_cwd = true,
+            },
+            open_file = {
+                resize_window = true,
+                window_picker = {
+                    chars = "asdfghjkl",
+                },
+            },
         },
     })
 
