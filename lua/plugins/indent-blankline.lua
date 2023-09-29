e9f179eb990c03175f8d476b281a18fe3fde3182
JJWRoeloffs <jelleroeloffs@gmail.com>
2023-09-29 13:34:13 +0200

    Update indent-blankline to behave as before update

--- lua/plugins/indent-blankline.lua
+++ lua/plugins/indent-blankline.lua
@@ -1,1 +1,5 @@
-return { "lukas-reineke/indent-blankline.nvim", main = "ibl", opts = {} }
+return {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    opts = { indent = { char = "│" }, scope = { enabled = false } },
+}
