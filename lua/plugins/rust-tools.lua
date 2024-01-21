9cd6d1fbebc46cf5cbb273e6d4cfaa74eddc00bd
JJWRoeloffs <jelleroeloffs@gmail.com>
2024-01-21 14:39:51 +0100

    Move from rust-tools to rustaceanvim

--- lua/plugins/rust-tools.lua
+++ lua/plugins/rust-tools.lua
@@ -18,56 +18,112 @@ local function setup_crates()
     vim.keymap.set("v", "<leader>rcU", crates.upgrade_crates, opts)
     vim.keymap.set("n", "<leader>rcA", crates.upgrade_all_crates, opts)
 
-    -- vim.keymap.set("n", "<leader>rce", crates.expand_plain_crate_to_inline_table, opts)
-    -- vim.keymap.set("n", "<leader>rcE", crates.extract_crate_into_table, opts)
-
     vim.keymap.set("n", "<leader>rcH", crates.open_homepage, opts)
     vim.keymap.set("n", "<leader>rcR", crates.open_repository, opts)
     vim.keymap.set("n", "<leader>rcD", crates.open_documentation, opts)
     vim.keymap.set("n", "<leader>rcC", crates.open_crates_io, opts)
 end
 
-local function on_attatch(client, bufnr)
+local function setup_rustacean()
+    local cmp = require("cmp")
+    cmp.setup({
+        snippet = {
+            expand = function(args)
+                require("luasnip").lsp_expand(args.body)
+            end,
+        },
+        mapping = cmp.mapping.preset.insert({
+            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+            ["<C-f>"] = cmp.mapping.scroll_docs(4),
+            ["<C-e>"] = cmp.mapping.abort(),
+            ["<C-y>"] = cmp.mapping.confirm({
+                behavior = cmp.ConfirmBehavior.Replace,
+                select = true,
+            }),
+        }),
+        sources = cmp.config.sources({
+            { name = "nvim_lsp" },
+            { name = "luasnip" },
+        }, {
+            { name = "buffer" },
+        }),
+    })
+end
+
+local function on_attach(client, bufnr)
+    require("mason-extensions").ensure_installed({ "codelldb" })
+    -- Additionally:
+    -- rustup component add rust-analyzer
+    -- rustup component add rust-src
+
+    local opts = { silent = true, buffer = bufnr }
     -- rustup component add rls rust-analysis rust-src
-    require("mason-extensions").ensure_installed({ "codelldb", "rust-analyzer" })
 
     local lsp_keybinds = require("jjw.lsp-keybinds")
     lsp_keybinds.set_keybinds(client, bufnr)
     lsp_keybinds.set_formatting(client, bufnr)
 
-    local rt = require("rust-tools")
+    vim.keymap.set("n", "<leader>rem", function()
+        vim.cmd.RustLsp("expandMacro")
+    end, opts)
 
-    vim.keymap.set("n", "<leader>rhs", rt.inlay_hints.set)
-    vim.keymap.set("n", "<leader>rhu", rt.inlay_hints.unset)
+    vim.keymap.set("n", "<leader>rmu", function()
+        vim.cmd.RustLsp({ "moveItem", "up" })
+    end, opts)
+    vim.keymap.set("n", "<leader>rmd", function()
+        vim.cmd.RustLsp({ "moveItem", "down" })
+    end, opts)
 
-    vim.keymap.set("n", "<leader>rem", rt.expand_macro.expand_macro)
+    vim.keymap.set("v", "<leader>rK", function()
+        vim.cmd.RustLsp({ "hover", "range" })
+    end, opts)
 
-    vim.keymap.set("n", "<leader>rmu", rt.move_item.move_item(true))
-    vim.keymap.set("n", "<leader>rmd", rt.move_item.move_item(false))
+    vim.keymap.set("n", "<leader>rct", function()
+        vim.cmd.RustLsp("openCargo")
+    end, opts)
 
-    vim.keymap.set("v", "<leader>rK", rt.hover_range.hover_range)
+    vim.keymap.set("n", "<leader>rmu", function()
+        vim.cmd.RustLsp("parentModule")
+    end, opts)
 
-    vim.keymap.set("n", "<leader>rct", rt.open_cargo_toml.open_cargo_toml)
+    vim.keymap.set("n", "<leader>rdb", function()
+        vim.cmd.RustLsp({ "debuggables", "last" })
+    end, opts)
 
-    vim.keymap.set("n", "<leader>rmu", rt.parent_module.parent_module)
+    vim.keymap.set("n", "<leader>ree", function()
+        vim.cmd.RustLsp("explainError")
+    end, opts)
 
-    vim.keymap.set("n", "<leader>rdb", rt.debuggables.debuggables)
+    setup_rustacean()
 end
 
 return {
     {
-        "simrat39/rust-tools.nvim",
+        "mrcjkb/rustaceanvim",
+        version = "^3", -- Recommended
         dependencies = {
-            { "neovim/nvim-lspconfig" },
-            { "nvim-lua/plenary.nvim" },
+            { "hrsh7th/nvim-cmp" },
+            { "hrsh7th/cmp-nvim-lsp" },
+            { "hrsh7th/cmp-buffer" },
+            { "hrsh7th/cmp-path" },
+            { "saadparwaiz1/cmp_luasnip" },
+            { "hrsh7th/cmp-nvim-lua" },
+            { "folke/lsp-colors.nvim" },
+
+            { "L3MON4D3/LuaSnip" },
+            { "rafamadriz/friendly-snippets" },
+
             { "mfussenegger/nvim-dap", config = require("plugins.nvim-dap").setup },
+
+            { "williamboman/mason.nvim", config = require("mason").setup },
+            require("plugins.telescope"),
         },
-        ft = { "rust", "toml" },
-        config = {
-            server = {
-                on_attach = on_attatch,
-            },
-        },
+        init = function()
+            vim.g.rustaceanvim = {
+                server = { on_attach = on_attach },
+            }
+        end,
+        ft = { "rust" },
     },
     {
         "saecki/crates.nvim",
