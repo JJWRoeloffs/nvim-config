3d1973fcf16027bdec3fb901a218917bec11846e
JJWRoeloffs <jelleroeloffs@gmail.com>
2024-08-01 11:28:20 +0200

    Reconfigure treesitter-context

--- lua/plugins/treesitter-context.lua
+++ lua/plugins/treesitter-context.lua
@@ -1,12 +1,21 @@
-return {
-    "nvim-treesitter/nvim-treesitter-context",
-    dependencies = {
-        require("plugins.treesitter"),
-    },
-    opts = {
+local function setup()
+    require("treesitter-context").setup({
         min_window_height = 20,
         mode = "topline",
         separator = "_",
         multiline_threshold = 4,
+        max_lines = 6,
+    })
+    vim.keymap.set("n", "<leader>tct", "<CMD>TSContextToggle<CR>")
+end
+
+return {
+    "nvim-treesitter/nvim-treesitter-context",
+    dependencies = {
+        {
+            "nvim-treesitter/nvim-treesitter",
+            config = require("plugins.treesitter").config,
+        },
     },
+    config = setup,
 }
